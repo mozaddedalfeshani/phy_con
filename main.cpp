cfe6213450114d9c7cd1d3fcd6e3ad9ea77ee9d7
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-11-22 22:28:52 +0600

    starting

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int test_case;
+    cin >> test_case;
+    for (int init = 1; init <= test_case;init++){
+        
+    }
+        return 0;
+}
